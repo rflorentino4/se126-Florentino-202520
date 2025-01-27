658bccd563af58f6ec0558cda5deab43757280a1
rflorentino4 <rflorentino@email.neit.edu>
2025-01-26 20:55:34 -0500

    completion of week3d2home

--- week3d2home/week3TextFileHandlingPART2.py
+++ week3d2home/week3TextFileHandlingPART2.py
@@ -3,18 +3,27 @@
 #SE126-2025.20
 #---------------------------------------------
 
+
 #DIRECTIONS: This in-class lab is a continuation of Lab #2, where instead of processing the data directly from the file, you will first store the data to lists (one list for every possible field in the text file records) and then use for loops to complete your printing and computing for cost.
 #---------------------------------------------
+
 
 #PROGRAM PROMPT: Your CIO (Chief Information Officer) has asked you to determine how much it would cost the company to replace all machines that are from 2016 and earlier. He plans on spending not more than $2,000 dollars for desktops and $1,500 for laptops.  Store the data from the file lab3a.csv into lists.  Then process the lists to reprint all of the file information (exactly as you did in Lab 2) and also produce an end report that lists the number of desktops that will be replaced, the cost to replace the desktops, the number of laptops that will be replaced, and the cost to replace the laptops.
 #---------------------------------------------
 
+
 #imports-----------------
 import csv
 
+
 #functions---------------
+def byeKT():
+    print(f"\n{'Have a good day KT :]' : ^65}\n")
+
 
 #main executing code-----
+
+
 #create lists for each POSSIBLE field in the text file records
 type = []
 brand = []
@@ -26,15 +35,14 @@ secondDisk = []
 os = []
 yr = []
 
-#initialize my counting variable
-totalRec = 0
 
 #connected to file-------
 with open("week3d2home/filehandling.csv") as csvfile: #don't forget to CHANGE the SLASH, you'll see a weird color if not
+    print(f'\n{"Type":9} {"Brand":8} {"CPU":4} {"RAM":4} {"1st Disk":10} {"No HDD":7} {"2nd Disk":9} {"OS":6} {"YR"}') #this can be wherever you want, as long as it's NOT in the FOR LOOP ++++ be care of how you use single & double quotes, must use both to differentiate (I think? this works tho)
+
     file = csv.reader(csvfile)
 
-    for rec in file: #occurs for every record/row in the textfile/2D list
-        totalRec += 1   
+    for rec in file: #occurs for every record/row in the textfile/2D list 
         #send type to list
         if rec[0] == "D":
             type.append("Desktop")
@@ -60,7 +68,7 @@ with open("week3d2home/filehandling.csv") as csvfile: #don't forget to CHANGE th
         
         #send the rest to list if no hdd is 1
         if int(rec[5]) == 1:
-            secondDisk.append("") #this will be empty because 1's don't have a second
+            secondDisk.append("-----") #this will be empty because 1's don't have a second disk
             os.append(rec[6])
             yr.append(rec[7])
 
@@ -70,23 +78,20 @@ with open("week3d2home/filehandling.csv") as csvfile: #don't forget to CHANGE th
             os.append(rec[7])
             yr.append(rec[8])
              
+
 #--disconnected from file--------------------------------------------
-
-#display final data (counting var)
-print(f"\nTotal records in the file: {len(type)}")
-
-print(f"{'Have a good day KT :]' : ^65}\n")        
-
-
-#processing data from lists using FOR loop
-
+      
+#processing data from lists using FOR loop to print all of the file information
 for index in range(0, len(type)): #this will run for as many i's/indexes there are in the list --incrementing by 1 each time
-    print(f"{type[index]:10} {brand[index]:10} {cpu[index]:5} {ram[index]:5} {firstDisk[index]:10} {nohdd[index]:10} {secondDisk[index]:10} {os[index]:5} {yr[index]:5}")
+    print(f"{type[index]:9} {brand[index]:8} {cpu[index]:4} {ram[index]:4} {firstDisk[index]:10} {str(nohdd[index]):7} {secondDisk[index]:9} {os[index]:6} {yr[index]}")
 
-#---
+
+#initialize counting variables
 oldDesktop = 0
 oldLaptop = 0
 
+
+#processing data from lists using FOR loop
 for index in range(0, len(yr)):
     if int(yr[index]) <= 16: #old
         if type[index] == "Desktop":
@@ -94,8 +99,14 @@ for index in range(0, len(yr)):
         elif type[index] == "Laptop":
             oldLaptop += 1
 
-print(f"\nNumber of desktops that need to be replaced: {oldDesktop}. This will cost you ${oldDesktop * 2000:.2f}")
-print(f"\nNumber of laptops that need to be replaced: {oldLaptop}. This will cost you ${oldLaptop * 1500:.2f}")
 
+#end report
+print("---------------------------------------------------------------------------")
+print(f"\nNumber of desktops that need to be replaced: {oldDesktop}. This will cost you ${oldDesktop * 2000:.2f}")
+print(f"\nNumber of laptops that need to be replaced: {oldLaptop}. This will cost you ${oldLaptop * 1500:9.2f}")
+print(f"\n**At a minimum, you will need ${oldDesktop * 2000 + oldLaptop * 1500:.2f} to replace all machines from 2016 and earlier.**")
+print("---------------------------------------------------------------------------")
 print(f"\nTotal records in the file: {len(type)}")
 
+#my goodbye func
+byeKT()
