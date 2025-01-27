a9e3f8e76a31f27cd8a7704074d23b80f1433cc1
rflorentino4 <rflorentino@email.neit.edu>
2025-01-27 11:17:21 -0500

    wipppppppppp

--- week4d1home/W4D1_SequentialSearchHOME.py
+++ week4d1home/W4D1_SequentialSearchHOME.py
@@ -1,5 +1,7 @@
-#WD1 - Sequential Searc
-
+#W4D1 Sequential Search
+#Romina Florentino 
+#SE126.04 - 2025
+#---------------------------------------------
 #PROGRAM PROMPT: We will continue to work with the class grades csv file, as used in the W3D2 demo. We will practice connecting to a file, storing the file data into parallel lists, and creating new data for each student record based on these lists. We will then build a sequental search program which wll allow us to find students in the file, and write data regardig the to a newly created file in our repository.
 
 #this file uses: class_grades.csv
@@ -60,10 +62,20 @@ for index in range(0, len(fName)):
     letAvg.append(l) #can also do: letAvg.append(letter(a)) BUT it is better to do it thi way when you need to reutilize ur vars
 
 #process the lists to display all student data back to the user
+print("\n\t\tPART ONE\n")
+print (f"{'Fname':12} {'Lname':13} {'T1':5} {'T2':5} {'T3':7}") #header print statements
+print("-----------------------------------------")
+
+for index in range(0, len(fName)): #print the data within these vars
+    print(f"{fName[index]:10}   {lName[index]:10}   {test1[index]:3}   {test2[index]:3}   {test3[index]:3}")
+
+print("\n************************************************************")
+
+print("\n\t\t\tPART TWO\n")
 print (f"{'Fname':12} {'Lname':13} {'T1':5} {'T2':5} {'T3':7} {'# Avg:':7} {'L Avg'}") #header print statements
 print("------------------------------------------------------------")
 
-for index in range(0, len(fName)):
+for index in range(0, len(fName)): #print the data within these vars
     print(f"{fName[index]:10}   {lName[index]:10}   {test1[index]:3}   {test2[index]:3}   {test3[index]:3}   {numAvg[index]:8.2f}   {letAvg[index]}")
 
 print("------------------------------------------------------------")
@@ -80,10 +92,11 @@ while answer == "y":
     #get search type from user
     print("\n\tSEARCH MENU OPTIONS")
     print("\n1. Search by LAST NAME")
-    print("2. Search by LETTER GRADE")
-    print("3. *EXIT*")
+    print("2. Search by FIRST NAME")
+    print("3. Search by LETTER GRADE")
+    print("4. *EXIT*")
 
-    searchType = input("\nEnter your search type [1-3]\n")
+    searchType = input("\nEnter your search type [1-4]\n")
 
     if searchType == "1":
         print("\t\nSEARCH BY LAST NAME")
@@ -91,13 +104,41 @@ while answer == "y":
         found = -1 #use bc it is an invalid index number, will use to check later to see if a student has been found
 
         #get search item from user
-        searchName = input("Enter the LAST NAME of the student you are searching for: \n")
+        searchLast = input("Enter the LAST NAME of the student you are searching for: \n")
 
 
         #perform search
         for index in range(0, len(lName)):
             #the FOR LOOP allows for/handles the "sequence" part -> from the beginning to the end
-            if searchName.lower() == lName[index].lower(): #must emphasize this bc the csv has different casing and human error is inevitable
+            if searchLast.lower() == lName[index].lower(): #must emphasize this bc the csv has different casing and human error is inevitable
+                #the IF STATEMENT allows for/handles the "search part"
+                found = index #make the found var the current index which can be used later 2 display
+
+        #display results
+        if found != -1: #if this is true, the last name has been found and can be displayed
+            print("test1")
+            print(f"Your search for {searchLast} was FOUND!")
+            print("test2")
+            print(f"{fName[found]:10}   {lName[found]:10}   {test1[found]:3}   {test2[found]:3}   {test3[found]:3}   {numAvg[found]:8.2f}   {letAvg[found]}")
+            print("Test3")
+        else:
+            print(f"Your search for {searchLast} was *NOT* FOUND!")
+            print("test4")
+            print("Check your spelling and try again.")
+
+    if searchType == "2":
+        print("\t\nSEARCH BY FIRST NAME")
+
+        found = -1 #use bc it is an invalid index number, will use to check later to see if a student has been found
+
+        #get search item from user
+        searchName = input("Enter the FIRST NAME of the student you are searching for: \n")
+
+
+        #perform search
+        for index in range(0, len(fName)):
+            #the FOR LOOP allows for/handles the "sequence" part -> from the beginning to the end
+            if searchName.lower() == fName[index].lower(): #must emphasize this bc the csv has different casing and human error is inevitable
                 #the IF STATEMENT allows for/handles the "search part"
                 found = index #make the found var the current index which can be used later 2 display
 
@@ -109,8 +150,7 @@ while answer == "y":
             print(f"Your search for {searchName} was *NOT* FOUND!")
             print("Check your spelling and try again.")
 
-
-    elif searchType == "2":
+    elif searchType == "3":
         print("\tSEARCH BY LETTER GRADE")
 
         found = [] #creates empty list to gather and store found index values
@@ -137,14 +177,15 @@ while answer == "y":
                 print(f"{fName[found[index]]:10}   {lName[found[index]]:10}   {test1[found[index]]:3}   {test2[found[index]]:3}   {test3[found[index]]:3}   {numAvg[found[index]]:8.2f}   {letAvg[found[index]]}")
 
 
-    elif searchType == "3":
+    elif searchType == "4":
         print("\tEXITING")
         answer = "n"
 
     else:
+        print("test5")
         print("*INVALID ENTRY!!*")
 
 
     #build a way out of the loop
-    if searchType == "1" or searchType == "2": #only used when user doesn't specify 3 to exit
+    if searchType == "1" or searchType == "2" or searchType =="3": #only used when user doesn't specify 3 to exit
         answer = input("\t\t\nWould you like to search again? [y/n]: \n").lower()
