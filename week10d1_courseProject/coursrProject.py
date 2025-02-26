ae7a301c8ed1e143ccee706b399c4730ae2f8b05
rflorentino4 <rflorentino@email.neit.edu>
2025-02-26 15:26:47 -0500

    wips

---
+++ week10d1_courseProject/coursrProject.py
@@ -0,0 +1,22 @@
+#W10 - Course Project
+#Romina Florentino
+#SE126.04 - 2025
+
+#---------------------------------------------
+#PROGRAM PROMPT:
+
+#IMPORTS--------------------------------------
+import csv
+
+#create lists for the following:
+
+#FUNCTIONS------------------------------------
+
+#CONNECTED TO FILE----------------------------
+with open("week10d1_courseProject\data.csv") as csvfile:
+    file = csv.reader(csvfile)
+
+    for rec in file:
+        
+
+#DISCONNECTED TO FILE-------------------------
